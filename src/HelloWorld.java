6eb0bdc6fca86fa05d8bea3df696d742c59c57a9
hslee <dlgytjdsla@naver.com>
2023-10-02 00:12:19 +0900

    feat: Java Command

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // printIn인줄 알았는데 printLn이네;
+        // Ln은 line의 약자이다.
+    }
+};
