d1eb9b2bb7251fcdc28520f93d08f11443a18fb1
hslee <dlgytjdsla@naver.com>
2023-10-02 15:36:26 +0900

    feat: Variables(String, Number);

---
+++ Java_study/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args){
+        int a; // 정수
+        a = 1;
+
+        System.out.println(a);
+
+        a = 2;
+        System.out.println(a);
+
+        double b; // 실수
+        b = 1.1;
+
+        System.out.println(b);
+
+
+        String c;
+        String d = "efgh";
+        c = "abcd";
+
+        System.out.println(c + d);
+    }
+}
