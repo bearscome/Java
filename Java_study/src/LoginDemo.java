19ff3d3001236e6a5c896659c22f45b678f30def
hslee <dlgytjdsla@naver.com>
2023-10-05 01:52:24 +0900

    feat: 분기  switch

--- Java_study/src/LoginDemo.java
+++ Java_study/src/LoginDemo.java
@@ -14,5 +14,17 @@ public class LoginDemo {
         } else {
             System.out.println("Wrong");
         }
+
+        switch (1) {
+            case 1:
+                System.out.println('1');
+                break;
+            case 2:
+                System.out.println('2');
+                break;
+            default:
+                System.out.println('3');
+                break;
+        }
     }
 }
