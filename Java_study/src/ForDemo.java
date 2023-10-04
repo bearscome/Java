0abc0ea37704c47572a8bb6209c7de7f83680957
hslee <dlgytjdsla@naver.com>
2023-10-05 02:31:01 +0900

    feat: loop (for)  - continue, break;

--- Java_study/src/ForDemo.java
+++ Java_study/src/ForDemo.java
@@ -2,6 +2,18 @@ public class ForDemo {
     public static void main(String[] args) {
         for (int i = 5; i < 20; i = i + 2) {
             System.out.println("IIII: " + i);
+
+            if(i == 9) {
+                System.out.println("contine: " + i);
+                continue;
+            }
+
+            System.out.println("for: " + i);
+
+
+            if(i == 13) {
+                break;
+            }
         }
     }
 }
