b4a693ddd4ed78d3ce728c241618dc0c85883d70
hslee <dlgytjdsla@naver.com>
2023-10-05 02:13:15 +0900

    feat: loop (for)  - for

---
+++ Java_study/src/ForDemo.java
@@ -0,0 +1,7 @@
+public class ForDemo {
+    public static void main(String[] args) {
+        for (int i = 5; i < 20; i = i + 2) {
+            System.out.println("IIII: " + i);
+        }
+    }
+}
