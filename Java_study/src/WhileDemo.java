d42a9f4f8f5bcdfe8453eb53f4b94722e7b7b7f7
hslee <dlgytjdsla@naver.com>
2023-10-05 02:03:58 +0900

    feat: loop (while)  - while

---
+++ Java_study/src/WhileDemo.java
@@ -0,0 +1,14 @@
+public class WhileDemo {
+    public static void main(String[] args) {
+//        while (true) {
+//            System.out.println("HI");
+//        };
+
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println("print: " + i);
+            i++;
+        }
+    }
+}
