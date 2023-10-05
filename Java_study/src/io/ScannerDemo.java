6c9cc09adec2ffb1423ca4f1deca55ab6a5f47de
hslee <dlgytjdsla@naver.com>
2023-10-05 18:57:54 +0900

    feat: 입출력 데모  - Scanner: 프로그램이 멈춘 후 사용자의 입력 값을 받아 처리

--- Java_study/src/io/ScannerDemo.java
+++ Java_study/src/io/ScannerDemo.java
@@ -9,7 +9,7 @@ public class ScannerDemo {
         // System.in = 사용자가 입력 한 값
         int i = sc.nextInt();
         // sc.nextInt() = 프로그램이 멈춤
-        // 엔터(공백)를 입력 시 자바가 실행되면서 사용자가 입력한 값이 i에 할당 된다.
+        // 엔터(공백)를  입력 시 자바가 실행되면서 사용자가 입력한 값이 i에 할당 된다.
 
         System.out.println(i*1000);
         sc.close();
