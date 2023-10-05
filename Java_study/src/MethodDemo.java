75aa3a0a6f4938865e308c3c04c7dd1b7a791713
hslee <dlgytjdsla@naver.com>
2023-10-05 16:25:54 +0900

    feat: 입출력 데모

--- Java_study/src/MethodDemo.java
+++ Java_study/src/MethodDemo.java
@@ -1,23 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MethodDemo {
-    public static int numbering(int _a, int _limit) {
+    public static int numbering(int _init, int _limit) {
         // public 몰라
         // static 몰라
         // int는 리턴 하는 타입 -> int를 리턴한다.
-        int i = _a;
+        int i = _init;
         int limit = _limit;
-        int ouput = 0;
+        int output = 0;
 
         while (i < limit) {
-            ouput += i;
+            output += i;
             i++;
         }
 
-        return ouput;
+        return output;
     }
 
     public static void main(String[] args) {
         // void는 리턴하는 값이 없다.
         int num = numbering(1,5);
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+            out.write(num);
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Fail Saved File");
+            throw new RuntimeException(e);
+        }
 
         System.out.println(num);
     }
