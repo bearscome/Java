59588393cb148b89dc6a8605941ac413957098ed
hslee <dlgytjdsla@naver.com>
2023-10-09 19:05:31 +0900

    feat: package  - import하여 다른 파일에 있는 class를 로드함

---
+++ Java_study/src/season2/object/package1/A.java
@@ -0,0 +1,7 @@
+package season2.object.package1;
+
+public class A {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java_study/src/season2/object/package2/B.java
@@ -0,0 +1,13 @@
+package season2.object.package2;
+
+import season2.object.package1.*;
+// 해당 패키지 안에 있는 모든 클래스를 가져온다.
+import season2.object.package1.A;
+// 패키지 안에 있는 A라는 클래스를 가져온다.
+
+
+public class B {
+    public static void main(String[] args) {
+        A a = new A();
+    }
+}
