e64fb33c8e431f09070b2a78a6130951c3fcb45b
hslee <dlgytjdsla@naver.com>
2023-10-19 01:10:03 +0900

    feat: 다향성  - 데이터 타입을 현재 인스터스할 클래스의 타입이 아닌 부모의 데이터 타입을 선언 할 수 있다.  - 데이터 타입이 부모 클래스의 데이터 타입이라면, 현재 메소드는 사용할 수 없다.  - 부모 클래스에서 오버라이딩 된 메소드는 사용 할 수 있다.

--- Java_study/src/season2/object/polymorphism/PolymorphismDemo1.java
+++ Java_study/src/season2/object/polymorphism/PolymorphismDemo1.java
@@ -1,14 +1,22 @@
 package season2.object.polymorphism;
 
 class A{
-    public Character x() {
-        return 'x';
+    public String x() {
+        return "A.x";
+    }
+
+    public String z() {
+        return "A.x";
     }
 }
 
 class B extends A {
-    public Character y() {
-        return 'y';
+    public String y() {
+        return "y";
+    }
+
+    public String x() {
+        return "B.x";
     }
 }
 
@@ -17,10 +25,23 @@ public class PolymorphismDemo1 {
         A obj = new B();
 
         obj.x();
-        obj.y();
+//        obj.y();
+        obj.z();
         /**
          * 인스턴스를 만들 때, 그 인스턴스의 데이터 타입은
          * 그 클래스의 부모 클래스가 될 수도 있다
+         */
+
+        System.out.println(obj.x());
+        /**
+         * 부모 클래스의 B가 아닌,
+         * 클래스 B에 정의된 메소드를 실행한다.
+         * 부모 클래스의 y 메소드가 정의되어 있지 않기 때문에,
+         * obj.y() 호출 시 오류를 리턴한다.
+         *
+         * 데이터 타입이 부모 클래스일 때,
+         * 현재 인스턴스된 메소드는 오버라이딩이 안된 경우라면
+         * 현재 인스턴스된 클래스의 메소드는 사용할 수 없다.
          */
     }
 }
