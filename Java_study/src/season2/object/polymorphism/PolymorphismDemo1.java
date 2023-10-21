8ba9a6fd4189cc8bdcb10e6a9175b162482d9de6
hslee <dlgytjdsla@naver.com>
2023-10-21 18:39:16 +0900

    feat: 다향성          /**           * 다양성을 통해 부모의 클래스를 받아 다른 인스턴스라도           * 동일한 부모의 메소드를 받아 사용할 수 있다.           * 만약 다른 데이터 타입을 가지고 있다면 execute의 함수에서 매개변수를 받을 때,           * 서로 다른 c1,c2의 데이터 타입을 정의하여 두 개의 함수를 만들어야 할 것이다.           */

--- Java_study/src/season2/object/polymorphism/PolymorphismDemo1.java
+++ Java_study/src/season2/object/polymorphism/PolymorphismDemo1.java
@@ -20,7 +20,52 @@ class B extends A {
     }
 }
 
+abstract class Calcurator {
+    int right, left;
+
+    public void setOperands(int _right, int _left) {
+        this.right = _right;
+        this.left = _left;
+    }
+
+    int _sum() {
+        return this.left + this.right;
+    }
+
+    abstract void sum();
+    abstract void avg();
+
+    public void run() {
+        sum();
+        avg();
+    }
+};
+
+class CaculaorPlus extends Calcurator {
+    public void sum(){
+        System.out.printf("+++++++++++++++++++++++++++++" + _sum());
+    }
+
+    public void avg() {
+        System.out.printf("CaculaorPlusCaculaorPlusCaculaorPlus" + (this.right -this.left / 2));
+    }
+}
+
+class CaculaorMinus extends Calcurator {
+    public void sum(){
+        System.out.printf("---------------" + _sum());
+    }
+
+    public void avg() {
+        System.out.printf("CaculaorPlusCaculaorPlusCaculaorPlus" + (this.right -this.left / 2));
+    }
+}
+
 public class PolymorphismDemo1 {
+    public static void execute(Calcurator cal) {
+        System.out.printf("ca1 run");
+        cal.run();
+    }
     public static void main(String[] args) {
         A obj = new B();
 
@@ -42,6 +87,22 @@ public class PolymorphismDemo1 {
          * 데이터 타입이 부모 클래스일 때,
          * 현재 인스턴스된 메소드는 오버라이딩이 안된 경우라면
          * 현재 인스턴스된 클래스의 메소드는 사용할 수 없다.
+         */
+
+        Calcurator c1 = new CaculaorPlus();
+        c1.setOperands(10,20);
+
+        Calcurator c2 = new CaculaorMinus();
+        c1.setOperands(30, 40);
+
+        execute(c1);
+        execute(c2);
+
+        /**
+         * 다양성을 통해 부모의 클래스를 받아 다른 인스턴스라도
+         * 동일한 부모의 메소드를 받아 사용할 수 있다.
+         * 만약 다른 데이터 타입을 가지고 있다면 execute의 함수에서 매개변수를 받을 때,
+         * 서로 다른 c1,c2의 데이터 타입을 정의하여 두 개의 함수를 만들어야 할 것이다.
          */
     }
 }
