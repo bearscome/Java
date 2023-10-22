a4eece50c954ad55e282ac88299c686f04fe7367
hslee <dlgytjdsla@naver.com>
2023-10-23 00:33:46 +0900

    feat: 예외 처리  - throws를 추가하여 사용자에게 에러처리를 넘길 수 있다.

--- Java_study/src/season2/object/exception/ThrowExceptionDemo.java
+++ Java_study/src/season2/object/exception/ThrowExceptionDemo.java
@@ -1,18 +1,30 @@
 package season2.object.exception;
 
+import java.io.*;
+
 class B {
-    void run() {
+    void run() throws FileNotFoundException, IOException {
     /* 만약 exception이 일어날 경우 B의 run에
      * try, catch문을 사용할 수 있지만, C 클래스에 넘길 수도 있다.
      * B -> C -> ThrowExceptionDemo으로 넘길 수 있다.
      */
+        bReader = new BufferedReader(new FileReader("out.txt"));
+        input = bReader.readLine();
+
+        System.out.println(input);
     }
 }
 
 class C {
     void run() {
         B b = new B();
-        b.run();
+        try {
+            b.run();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 public class ThrowExceptionDemo {
