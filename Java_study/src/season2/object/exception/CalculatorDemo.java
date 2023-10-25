16b0eec80b4702f14f614b17356bda71c8bc0578
hslee <dlgytjdsla@naver.com>
2023-10-25 23:05:19 +0900

    refactor: Exception

--- Java_study/src/season2/object/exception/CalculatorDemo.java
+++ Java_study/src/season2/object/exception/CalculatorDemo.java
@@ -3,11 +3,23 @@ package season2.object.exception;
 class Calculaotr {
     int left, right;
     public void setOprands(int _left, int _right) {
+//        if(_right < 1) {
+//            throw new IllegalAccessException("두 번째 인자는 1보다 커야합니다");
+//        };
+//        sum, minus일 경우 두 번쨰 인자가 0으로 들어올 수 있으니
+//        division에서 exception을 던지는게 좋음
+
        this.left = _left;
        this.right = _right;
     }
 
     public void division() {
+        if(this.right < 1) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            // Exception이 발견되면 catch문을 실행하고 해당 함수를 빠져나온다.
+        };
+
+
         try {
             System.out.println("계산된 값은: ");
             System.out.println(this.left / this.right);
@@ -33,6 +45,11 @@ public class CalculatorDemo {
     public static void main(String[] args) {
         Calculaotr c1 = new Calculaotr();
         c1.setOprands(10, 0);
-        c1.division();
+
+        try {
+            c1.division();
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
