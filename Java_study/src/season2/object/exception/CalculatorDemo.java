403cf0ae0336419b0ae9d91d60aeaadd13c50c11
hslee <dlgytjdsla@naver.com>
2023-12-10 17:01:33 +0900

    feat: 사용자 정의 예외  - uncheck에러는 상속 받아 사용자가 예외 처리를 할 수 있음   -> RuntimeException, ArithmeticException, ...

--- Java_study/src/season2/object/exception/CalculatorDemo.java
+++ Java_study/src/season2/object/exception/CalculatorDemo.java
@@ -1,52 +1,28 @@
 package season2.object.exception;
 
+//class DivideException extends RuntimeException {
+class DivideException extends ArithmeticException {
+    DivideException() {
+        super();
+    }
+
+    DivideException(String msg) {
+        super(msg);
+    }
+}
+
 class Calculaotr {
     int left, right;
     public void setOprands(int _left, int _right) {
-//        if(_right < 1) {
-//            throw new IllegalAccessException("두 번째 인자는 1보다 커야합니다");
-//        };
-//        sum, minus일 경우 두 번쨰 인자가 0으로 들어올 수 있으니
-//        division에서 exception을 던지는게 좋음
-
        this.left = _left;
        this.right = _right;
     }
 
     public void division() {
         if(this.right < 1) {
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
-            // Exception이 발견되면 catch문을 실행하고 해당 함수를 빠져나온다.
-
-
-//            예외	사용해야 할 상황
-//            IllegalArgumentException	매개변수가 의도하지 않은 상황을 유발시킬 때
-//            IllegalStateException	메소드를 호출하기 위한 상태가 아닐 때
-//            NullPointerException	매개 변수 값이 null 일 때
-//            IndexOutOfBoundsException	인덱스 매개 변수 값이 범위를 벗어날 때
-//            ArithmeticException	산술적인 연산에 오류가 있을 때
+            throw new DivideException("0으로 나눌 수 없습니다.");
         };
-
-
-        try {
-            System.out.println("계산된 값은: ");
-            System.out.println(this.left / this.right);
-            System.out.println("입니다.");
-        } catch (Exception e) {
-            System.out.println("오류가 발생했습니다" + e.getMessage());
-            // 어떤 오류? (/by zero)
-            System.out.println("오류가 발생했습니다" + e.toString());
-            // 어떤 exception(java.lang.ArithmeticException), 어떤 오류
-            System.out.println("오류가 발생했습니다" + e.getStackTrace());
-            // 예외가 발생했을 때 실행되는 로직
-        }
-
-//        계산된 값은:
-//        오류가 발생했습니다/ by zero
-
-
         System.out.println("DIVIDE END");
-//        catch문 이후에 있는 함수 실행
     }
 }
 public class CalculatorDemo {
