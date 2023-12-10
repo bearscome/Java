dbd90e0132800a5a3ba4b440b33595a06902bbfe
hslee <dlgytjdsla@naver.com>
2023-12-10 23:23:10 +0900

    feat: Object.clone  - 모든 class의 조상은 Object이다.  - 모든 class는 Object의 methode를 공통적으로 가지고 있다.  - 메소드를 필요에 따라 오버라이등을 통해 동작을 변경할 수있다.

--- Java_study/src/season2/object/progenitor/ObjectDemo.java
+++ Java_study/src/season2/object/progenitor/ObjectDemo.java
@@ -1,29 +1,28 @@
 package season2.object.progenitor;
 
-class Student {
+class Student implements Cloneable{
+    // Cloneable의 인터페이스는 비어있지만 자바 버츄얼 머신이 해당 인터페이스를 읽어
+    // clone이 가능한 class라는 것을 명시해주는 것이다.
     String name;
 
     Student(String name) {
         this.name = name;
     }
 
-    public boolean equals(Object obj) {
-        // obj의타입이 Student가 아닐 경우는 Object로 변경하여 전달 받는다..
-
-        Student s = (Student) obj;
-        return this.name == s.name;
-    }
-
-    public boolean equals(Student obj) {
-        return this.name == obj.name;
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
 
 public class ObjectDemo {
     public static void main(String[] args) {
         Student s1 = new Student("1");
-        Student s2 = new Student("1");
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
+        try {
+           Student s2 = (Student) s1.clone();
+            System.out.println(s1.name);
+            System.out.println(s2.name);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
