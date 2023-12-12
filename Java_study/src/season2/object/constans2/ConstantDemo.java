5086d6f15fcdaa9ef913a261e281f5bde63cd51e
hslee <dlgytjdsla@naver.com>
2023-12-13 00:48:25 +0900

    feat: enum  - enum은 열거형이며, 열거형은 상수들을 저장한다.  - 값들이 변경되지 않도록 보장한다.  - 열거형은 자체가 class이기 때문에 생성자, 필드 ,메소드를 가지고 있어 상수 기능뿐만 아니라 더 많은 기능을 할 수 있다.

--- Java_study/src/season2/object/constans2/ConstantDemo.java
+++ Java_study/src/season2/object/constans2/ConstantDemo.java
@@ -9,7 +9,10 @@ package season2.object.constans2;
 enum Fruit {
     APPLE("red"), PEACH("pink"), BANANA("yellow");
     // 생성자를 호출할 때 매개변수를 넘겨 생성자는 인자를 받아 설정한다.
-    public String color;
+    private String color;
+    public String getColor() {
+        return this.color;
+    };
     Fruit(String color) {
         // 상수를 하나를 만들 때마다 인스턴스의 생성자가 호출된다.
         this.color = color;
@@ -42,18 +45,25 @@ public class ConstantDemo {
 
 
     public static void main(String[] args) {
+
+        for(Fruit f : Fruit.values()) {
+            System.out.println("FFF: " +f);
+            // enum은 열거형 타입이기 때문에 for in 가능
+            // 마치 배열처럼 상수를 하나씩 꺼내어 처리할 수 있다.
+        }
+
         Fruit type = Fruit.APPLE;
         switch (type) {
             // switch조건은 데이터 타입이 제한적이다 (int, char, byte, String, ...)
             // switch의 type은 Fruit의 타입이다.
             case APPLE:
-                System.out.println(57+"kcal, color " + Fruit.APPLE.color);
+                System.out.println(57+"kcal, color " + Fruit.APPLE.getColor());
                 break;
             case PEACH:
-                System.out.println(34+"kcal, color" + Fruit.PEACH.color);
+                System.out.println(34+"kcal, color" + Fruit.PEACH.getColor());
                 break;
             case BANANA:
-                System.out.println(93+"kcal, color " + Fruit.BANANA.color);
+                System.out.println(93+"kcal, color " + Fruit.BANANA.getColor());
                 break;
 
         }
