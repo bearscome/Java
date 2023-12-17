af4e7cca132545e8dd97af42bafd2955c35333f6
hslee <dlgytjdsla@naver.com>
2023-12-18 00:31:17 +0900

    feat: 참조
    - 기본 데이터 형은 변수를 복제한다.
      -> a와 b는 서로 영향 받지 않는다.
    
     - new 연산자의 데이터 형은 변수를 참조 한다.
      -> b에 a의 변수를 대입할 경우 a와 b 모두 영향을 받는다.
      -> 주소 값을 참조하는 것 같다. 마치 JS랑 동일한 듯

---
+++ Java_study/src/season2/object/reference/ReferenceDemo1.java
@@ -0,0 +1,35 @@
+package season2.object.reference;
+
+class A {
+    public int id;
+    A(int id) {
+        this.id = id;
+    }
+}
+
+public class ReferenceDemo1 {
+    public static void runValue() {
+        int a = 1;
+        int b = a;
+        b = 2;
+        System.out.println("runValue, " + a);
+        // 기본 데이터 타입은 복제가 일어나기 때문에
+        // a, b를 변경해도 영향을 받지 않는다.
+    }
+
+    public static void runReference() {
+        A a = new A(1);
+        A b = a;
+//        b.id = 2;
+        a.id = 2;
+        System.out.println("runReference, " + a.id);
+        // 인스턴스를 참조할 수 있는 주소 값만 가지고 있기 떄문에
+        // a , b의 id를 변경하면 해당 두 변수가 영향을 받는다. (메모리 주소?)
+        // 자바스크립트 객체와 동일한 거 같다.
+    }
+
+    public static void main(String[] args) {
+        runValue();
+        runReference();
+    }
+}
