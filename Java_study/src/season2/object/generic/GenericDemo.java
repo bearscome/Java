9c6a4c6007b951977528e16c24a9850ff3bdd3fc
hslee <dlgytjdsla@naver.com>
2023-12-19 02:12:13 +0900

    feat: generic
    제네릭 제한
     - 제네릭에 제한된 래퍼런스만 전달 받고 싶다면 extends를 통해 래퍼런스를 제한할 수 있다.
     - extends를 통해 래퍼런스를 제한 할 시 제한한 래퍼런스와 자식만 전달할 수 있다.
     - extends를 통해 래퍼런스를 추가했다면 해당 래퍼런스의 메소드를 사용할 수 있다.
    
     - super를 사용할 경우 해당 레퍼런스를 제외 한 모든 레퍼랜스를 전달할 수 있다.
    
     -

--- Java_study/src/season2/object/generic/GenericDemo.java
+++ Java_study/src/season2/object/generic/GenericDemo.java
@@ -1,36 +1,46 @@
 package season2.object.generic;
+//interface InterFaceInfo {
+//    int getLevel();
+//}
+//
+//class EmployeeInfo2 implements InterFaceInfo {
+//    int level;
+//    EmployeeInfo2(int level) {
+//        this.level = level;
+//    }
+//    public int getLevel() {
+//        return this.level;
+//    }
+//}
 
-class EmployeeInfo{
+abstract class Info {
+    public abstract int getLevel();
+    // abstract는 추상화 클래스, 메소드
+};
+
+class EmployeeInfo extends Info {
     public int rank;
-    EmployeeInfo(int rank){ this.rank = rank; }
-}
-class Person<T, S>{
-    // 복수의 제네릭이 필요한 경우 콤마가 필요하며, 네이밍이 달라야한다.
-    // 제네릭은 레퍼런스 타입(참조형)만 올 수 있다. (기본 데이터[int, char] 타입은 못 온다.)
-    // 기본 데이터 타입은 wrapper 클래스(오브젝트)를 사용하면 된다.
-    // int -> Integer, double -> Double
-    public T info;
-    public S id;
-    Person(T info, S id){
-        this.info = info;
-        this.id = id;
+    EmployeeInfo(int rank){ this.rank = rank; };
+    public int getLevel() {
+        return this.rank;
     };
-
-    public <U> void printInfo(U info) {
-        // 제네릭은 클래스 레벨에서만 사용하지 않아도 된다.
-        // 메소드 레벨에서도 사용이 가능하다.
-        System.out.println(info);
-    }
-}
+};
+class Person<T extends Info>{
+    // 제네릭을 사용할 경우 제넥릭의 래퍼런스를 제한하고 싶다면
+    // extends Object를 사용하면 해당 Object와 Object의 자식만 제네릭으로 사용이 가능하다.
+    // 제네릭에서 extends는 해당 래퍼런스의 부모를 가르키는 것이다.
+    // 제네릭에서 super는 해당 래퍼런스를 제외한 모든 래퍼런스를 받는 것이다 (실용성이 거의 없음)
+    public T info;
+    Person(T info) {
+        this.info = info;
+        info.getLevel();
+        // 제네릭에서 extends info를 했기 때문에 해당 메소드 사용할 수 있다.
+    };
+};
 public class GenericDemo {
     public static void main(String[] args) {
-//        Integer id = new Integer(3); // new Integer는 Java 9부터 사용하지 않는 생성자이다.
-        EmployeeInfo e = new EmployeeInfo(1);
-        Integer id = 1;
-        Person p1 = new Person(e, id);
-        // 변수에 선언된 데이터 타입이 있으면 제네릭 타입 생략 가능
-
-//        p1.<EmployeeInfo>printInfo(e); -> 타입 생략이 가능하다
-        p1.printInfo(e);
+        Person<EmployeeInfo> p1 = new Person<EmployeeInfo>(new EmployeeInfo(1));
+//        Person<String> p2 = new Person<String>("부장");
+        // String은 Info의 자식이 아니기 때문에 에러가 나타난다.
     }
 }
