55927b41cae58e6f4bc8aedc96e9f61edb054bfe
hslee <dlgytjdsla@naver.com>
2023-12-19 00:44:33 +0900

    feat: generic
    타입 선언부와 생성자의 제네릭은 동일해야 한다.

---
+++ Java_study/src/season2/object/generic/GenericDemo.java
@@ -0,0 +1,12 @@
+package season2.object.generic;
+
+class Person<T> {
+    public T info;
+}
+
+public class GenericDemo {
+    public static void main(String[] args) {
+        Person<String> p1 = new Person<String>();
+        Person<StringBuilder> p2 = new Person<StringBuilder>();
+    }
+}
