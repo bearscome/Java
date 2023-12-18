956bec9a93acab6e0bc15eefa6dfb66790964e05
hslee <dlgytjdsla@naver.com>
2023-12-19 01:33:00 +0900

    feat: generic
    복수의 generic 콤마로 구분하며 네이밍이 달라야 한다.
     - <T, S>
    제네릭의 타입은 래퍼런스타입(참조형)만 올 수 있다.
     - int, char 사용 불가
    제넥릭에서 기본 데이터 타입을 사용하고 있으면 해당 타입의 wrapper타입을 사용해야 한다.
     - int -> Integer
     - double -> Double

--- Java_study/src/season2/object/generic/GenericDemo.java
+++ Java_study/src/season2/object/generic/GenericDemo.java
@@ -1,22 +1,26 @@
 package season2.object.generic;
 
-class StudentInfo{
-    public int grade;
-    StudentInfo(int grade){ this.grade = grade; }
-}
 class EmployeeInfo{
     public int rank;
     EmployeeInfo(int rank){ this.rank = rank; }
 }
-class Person{
-    public Object info;
-    Person(Object info){ this.info = info; }
+class Person<T, S>{
+    // 복수의 제네릭이 필요한 경우 콤마가 필요하며, 네이밍이 달라야한다.
+    // 제네릭은 레퍼런스 타입(참조형)만 올 수 있다. (기본 데이터[int, char] 타입은 못 온다.)
+    // 기본 데이터 타입은 wrapper 클래스(오브젝트)를 사용하면 된다.
+    // int -> Integer, double -> Double
+    public T info;
+    public S id;
+    Person(T info, S id){
+        this.info = info;
+        this.id = id;
+    };
 }
 public class GenericDemo {
     public static void main(String[] args) {
-        Person p1 = new Person("부장");
-        // Person의 Object의 타입이 들어가기 때문에 타입에러가 안난다
-        // Object이기 때문에 type이 안전하지 않다.
-        EmployeeInfo ei = (EmployeeInfo) p1.info;
+//        Integer id = new Integer(3); // new Integer는 Java 9부터 사용하지 않는 생성자이다.
+        Integer id = 1;
+        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(new EmployeeInfo(1), id);
+        System.out.println(p1.id.intValue());
     }
 }
