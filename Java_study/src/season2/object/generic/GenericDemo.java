11e4f46a2798f3158a07fa9be30ad50d1923b3d3
hslee <dlgytjdsla@naver.com>
2023-12-19 01:18:09 +0900

    feat: generic
    p1.info를 형변환 EmployeeInfo로 하면 이걸 컴파일시에 에러를 못잡는 경우가 발생한다.
    
    제네릭은 컴 파일 단계에서 오류를 검출하기 떄문에 타입의 안전하다.

--- Java_study/src/season2/object/generic/GenericDemo.java
+++ Java_study/src/season2/object/generic/GenericDemo.java
@@ -4,25 +4,19 @@ class StudentInfo{
     public int grade;
     StudentInfo(int grade){ this.grade = grade; }
 }
-class StudentPerson{
-    public StudentInfo info;
-    StudentPerson(StudentInfo info){ this.info = info; }
-}
 class EmployeeInfo{
     public int rank;
     EmployeeInfo(int rank){ this.rank = rank; }
 }
-class EmployeePerson{
-    public EmployeeInfo info;
-    EmployeePerson(EmployeeInfo info){ this.info = info; }
+class Person{
+    public Object info;
+    Person(Object info){ this.info = info; }
 }
 public class GenericDemo {
     public static void main(String[] args) {
-        StudentInfo si = new StudentInfo(2);
-        StudentPerson sp = new StudentPerson(si);
-        System.out.println(sp.info.grade); // 2
-        EmployeeInfo ei = new EmployeeInfo(1);
-        EmployeePerson ep = new EmployeePerson(ei);
-        System.out.println(ep.info.rank); // 1
+        Person p1 = new Person("부장");
+        // Person의 Object의 타입이 들어가기 때문에 타입에러가 안난다
+        // Object이기 때문에 type이 안전하지 않다.
+        EmployeeInfo ei = (EmployeeInfo) p1.info;
     }
 }
