d5000970ac9edbed408a858d526922c0027995fb
hs.lee1 <hs.lee1@altimedia.com>
2024-12-15 22:34:38 +0900

    java variable type

---
+++ younghanKim/younghanKim/src/variable/Var7.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var7  {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리언(boolean) true, false 입력 가능
+        char d = 'a'; // 문자 하나
+        String e = "Hello java"; // 문자열, 문자열을 다루기 위한 특별한 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+        // Java에서 리터럴(literal)은 개발자가 직접 넣은 고정 된 값
+    }
+}
