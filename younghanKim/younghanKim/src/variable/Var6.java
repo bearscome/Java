734740fcc8a506a7137b650748b01ab5dc1935a4
hs.lee1 <hs.lee1@altimedia.com>
2024-12-15 21:48:38 +0900

    java Initialize variable

---
+++ younghanKim/younghanKim/src/variable/Var6.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a; // java: variable a might not have been initialized
+        // java의 지역 변수는 초기화를 강제한다
+//        System.out.println(a);
+    }
+}
