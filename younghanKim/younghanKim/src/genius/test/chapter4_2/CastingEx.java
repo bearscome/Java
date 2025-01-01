67afdb1840a4aa45d83c90552b2f24ffb80e01ee
bearscome <dlgytjdsla@naver.com>
2025-01-01 22:29:10 +0900

    java object 상속 및 형변환
    부모 클래스를 상속 받으려면 자식 클래스에 extends를 통해서 상속 받아야 한다.
    부모 클래스의 필드 및 메소드를 상속 받을 수 있다.
    
    자식 클래스가 부모 클래스 타입을 사용할 수 있으나,
    부모 클래스가 자식 클래스 타입을 사용 할 수 없다.
    
    부모 클래스가 자식 클래스의 타입을 사용하려면 형변환을 통해야 한다.

--- younghanKim/younghanKim/src/genius/test/chapter4_2/CastingEx.java
+++ younghanKim/younghanKim/src/genius/test/chapter4_2/CastingEx.java
@@ -17,7 +17,7 @@ public class CastingEx {
         // 자식 클래스 타입에 부모 클래스 대입 불가X
         SmartPhone sp2 = (SmartPhone) p; // 형 변환을 통해서 타입 변경 가능
 
-        sp2.installApp();;
-//        p2.installApp();
+        sp2.installApp();
+        p2.installApp();
     }
 }
