5294820bdd30f45bc176ae18ae9d5274ff5c3392
bearscome <dlgytjdsla@naver.com>
2025-02-25 23:42:19 +0900

    기본편: 절차 지향 프로그래밍 - 메서드 추출
    각각의 기능을 메서드로 만든 덕분에 "중복 제거", "변경 영향 범위", "메서드 이름추가"로 모듈화가 되었다.
    
    현재는 절차 지향으로 되어 있어서 데이터 관리와, 데이터 핸들링이 나눠져 있다.
    만약 데이터가 추가되거나 삭제되는 경우 두 개의 파일을 동시에 수정해야한다.
    객체 지향은 절차 지향의 단점을 보완하기 위해 나왔다.

--- younghanKim/younghanKim-basic/src/oop1/MusicPlayerMain2.java
+++ younghanKim/younghanKim-basic/src/oop1/MusicPlayerMain2.java
@@ -4,22 +4,44 @@ public class MusicPlayerMain2 {
     public static void main(String[] args) {
         MusicPlayerData MusicPlayerData = new MusicPlayerData();
 
+        on(MusicPlayerData);
+
+        // 볼륨증가
+        MusicPlayerData.volume++;
+        System.out.println("음악 플레이어 볼륨: " + MusicPlayerData.volume);
+
+        volumeUp(MusicPlayerData);
+        volumeUp(MusicPlayerData);
+        volumeDown(MusicPlayerData);
+        showStatus(MusicPlayerData);
+        off(MusicPlayerData);
+    }
+
+    static void on (MusicPlayerData MusicPlayerData) {
         // 음악 플레이어 켜기
         MusicPlayerData.isOn  = true;
         System.out.println("음악 플레이어를 시작합니다.");
+    }
 
+    static void off (MusicPlayerData MusicPlayerData) {
+        // 음악 플레이어 끄기
+        MusicPlayerData.isOn= false;
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+
+    static void volumeUp (MusicPlayerData MusicPlayerData) {
         // 볼륨증가
         MusicPlayerData.volume++;
         System.out.println("음악 플레이어 볼륨: " + MusicPlayerData.volume);
+    }
 
-        // 볼륨증가
-        MusicPlayerData.volume++;
-        System.out.println("음악 플레이어 볼륨: " + MusicPlayerData.volume);
-
+    static void volumeDown (MusicPlayerData MusicPlayerData) {
         // 볼륨감소
         MusicPlayerData.volume--;
         System.out.println("음악 플레이어 볼륨: " + MusicPlayerData.volume);
+    }
 
+    static void showStatus (MusicPlayerData MusicPlayerData) {
         // 음악 플레이어 상태
         System.out.println("음악 플레이어 상태 확인");
         if(MusicPlayerData.isOn) {
@@ -28,11 +50,5 @@ public class MusicPlayerMain2 {
         else {
             System.out.println("음악 플레이어 OFF");
         }
-        
-        // 음악 플레이어 끄기
-        MusicPlayerData.isOn= false;
-        System.out.println("음악 플레이어를 종료합니다.");
-        
-
     }
 }
