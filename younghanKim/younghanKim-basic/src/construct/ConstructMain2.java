1b69cc1ec8906d7f51ef20388f5ab3f3dc1501ba
bearscome <dlgytjdsla@naver.com>
2025-02-27 00:46:13 +0900

    기본편: 생성자 - 오버로딩과 this()
    여러 생성자를 생성하여 파라미터에 맞게 인스턴스 생성이 가능하다.
    
    this는 인스턴스 자신의 참조 값을 가르킨다
    this를 호출한 경우 생성자 내부에서 자신의 생성자를 호출 할 수 있다
    this는 생성자 코드의 첫 줄에만 작성할 수 있다.

---
+++ younghanKim/younghanKim-basic/src/construct/ConstructMain2.java
@@ -0,0 +1,14 @@
+package construct;
+
+public class ConstructMain2 {
+    public static void main(String[] args) {
+        MemberConstruct member1 = new MemberConstruct("user1", 15);
+        MemberConstruct member2 = new MemberConstruct("user2", 12, 90);
+
+        MemberConstruct[] memberInits = {member1, member2};
+
+        for (MemberConstruct s : memberInits) {
+            System.out.println("이름: " + s.name + " 나이: " + s.age + " 점수: " + s.grade);
+        }
+    }
+}
--- younghanKim/younghanKim-basic/src/construct/MemberConstruct.java
+++ younghanKim/younghanKim-basic/src/construct/MemberConstruct.java
@@ -5,6 +5,15 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name, int age) {
+       this(name, age, 50);
+        // this를 호출한 경우 생성자 내부에서 자신의 생성자를 호출 할 수 있다
+        // this는 인스턴스 자신의 참조 값을 가르킨다
+        // this는 생성자 코드의 첫줄에만 작성할 수 있다.
+        System.out.println("1");
+
+    }
+
     MemberConstruct(String name,int age, int grade) {
         System.out.println("생성자 호출 name = " + name + " age: " + age + " grade: " + grade);
         this.name = name;
