cc75491b4bbf0955e6ea23d981f54de67d9f3c01
bearscome <dlgytjdsla@naver.com>
2025-03-02 23:03:40 +0900

    기본편: 접근 제어자 - 접근 제어자 이해2
    class 내부에서만 접근이 가능하도록 하는 접근 제어자는 private이다.

--- younghanKim/younghanKim-basic/src/access/SpeakerMain.java
+++ younghanKim/younghanKim-basic/src/access/SpeakerMain.java
@@ -12,7 +12,7 @@ public class SpeakerMain {
         speaker.showVolume();
 
         // 필드에 직접 접근
-        speaker.volume = 200;
+//        speaker.volume = 200;
         speaker.showVolume();
     }
 }
