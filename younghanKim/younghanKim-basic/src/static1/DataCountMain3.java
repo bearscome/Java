28fe512251e63dbf2a5ee0bcb38c17bb36a90efc
bearscome <dlgytjdsla@naver.com>
2025-03-09 21:41:40 +0900

    기본편: 자바 메모리 구조와 static - static 변수3
    변수와 생명주기
     1. 지역 변수(매개 변수): 스택 영역에 있는 스택 프레임 안에 보관, 메서드가 종료되면 스택 프레임도 제거되는데 이때 해당 지역 변수도 제거 된다.
     2. 인스턴스 변수: 힙 영역에 보관, GC가 발생하기 전 까지는 생존
     3. 클래스 변수: 메서드 영역의 static영역에 보관, 프로그램 전체에서 사용하는 공용 공간 JVM이 종료되기 전까지 생존
    
     정전 변수 접근
     1. Class로 접근
     2. 인스턴스로 접근
      - 해당 방법은 추천하지 않으며, 코드를 읽을 때 인스턴스 변수로 보임

--- younghanKim/younghanKim-basic/src/static1/DataCountMain3.java
+++ younghanKim/younghanKim-basic/src/static1/DataCountMain3.java
@@ -10,5 +10,13 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count = " + Data3.count);
+
+        // 추가
+        // 인스턴스를 통한 접근
+        // 스테틱 변수인 경우 힙 영역에서 메소드 영역으로 변수 확인하로 감
+        // 정적 변수의 경우 인스턴스를 통한 접근은 추천하지 않는디ㅏ.
+        // 코드를 읽을 때 마치 인스턴스 변수에 접근하는 것처럼 보일 수 있다
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
     }
 }
