0100bbb4140d1b720428e7a957bf632b3c3d7b79
bearscome <dlgytjdsla@naver.com>
2025-03-10 00:33:24 +0900

    기본편: 자바 메모리 구조와 static - static 메서드 3
    - 인스턴스 메서드: static이 붙지 않은 메서드
    - 클래스 메서드: static이 붙은 메서드
     -- 클래스 메서드, 정적 메서드, static메서드 등으로 불림
    
    - 정적 메서드는 유틸리티성 메서드에 자주 사용 됌
    
    - static import
     -- import static package.*
     -- import static package.method
    
    - main method
     -- 인스턴스를 생성을 안 했는데 자바가 main을 실행
     -- main을 실행 할 때 static 메소드를 만들었는데, main이 static이기 때문

--- younghanKim/younghanKim-basic/src/static2/DecoDataMain.java
+++ younghanKim/younghanKim-basic/src/static2/DecoDataMain.java
@@ -1,5 +1,8 @@
 package static2;
 
+// import static static2.DecoData.*;
+// import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
