7cd8e8b3ad0df38e8f596004771e4a377e1ffa68
bearscome <dlgytjdsla@naver.com>
2025-03-13 23:08:42 +0900

    기본편: final - final 변수와 상수1
    - final 변수는 한 번만 할당 가능
    - 매개변수에 final이 붙으면 메서드 내부에서 매게변수 값을 변경 할 수 없음
    - 상수 선언은 static final type key = value

---
+++ younghanKim/younghanKim-basic/src/final1/FieldInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class FieldInit {
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+//    public FieldInit(int value) {
+    // this.value = value; 초기 값이 있는 경우 값 할당 불 가능
+//    }
+}
