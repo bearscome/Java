07e826f50adef8ec2b63d3c3bf856d8304f737f6
bearscome <dlgytjdsla@naver.com>
2025-03-16 21:44:09 +0900

    기본편: 다형성 2 - 인터페이스
    순수 추상 클래스를 좀 더 편하게 사용할 수 있는 기능 제공
    implements(구현)으로 확장
    
    추상 클래스
     - 일반 메서드
     - 추상 메서드
     - 멤버 변수
    순수 추상 클래스
     - 추상 메서드
     - 맴버 변수
    인터페이스
     - 추상 메서드
     - 상수

--- younghanKim/younghanKim-basic/src/poly/ex5/InterfaceAnimal.java
+++ younghanKim/younghanKim-basic/src/poly/ex5/InterfaceAnimal.java
@@ -1,7 +1,9 @@
 package poly.ex5;
 
 public interface InterfaceAnimal {
-    void sound(); // public abstract
+    double MY_PI = 3.14; // public static final 생략
+
+    void sound(); // public abstract 생략
 
     void move();
 }
